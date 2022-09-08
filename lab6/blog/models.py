3e9748c6908c23cd60131b9a0ae506bfe911caa7
YukioNice <mh.dania2015@gmail.com>
2022-09-08 14:41:18 +0300

    1

--- lab6/blog/models.py
+++ lab6/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Post(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
